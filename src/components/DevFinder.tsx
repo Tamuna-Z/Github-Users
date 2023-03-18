f3fd0a6e08170537eb876f2c3b4eb33cfd9c256a
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-03-18 14:51:07 +0400

    write useState for data and useEffect function

--- src/components/DevFinder.tsx
+++ src/components/DevFinder.tsx
@@ -3,15 +3,37 @@ import {useState,useEffect} from 'react';
 
 function DevFinder(){
     const apiURL= 'https://api.github.com/users/'
+    const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
     const [searchedWord, setSearchedWord] = useState<string>('octocat');
     const[avatar,setAvatar]=useState();
     const [acountCheck,setAcountCheck]=useState<boolean>(false);
+    const [name,setName]=useState<string|null>(null);
+    const[joined,setJoined]=useState<string>('');
+    const [bio,setBio]=useState<string | null>(null);
+    const [repos, setRepos]=useState<string | null>(null);
+    const [followers,setFollowers]=useState<string | null>(null);
+    const[following, setFollowing]=useState<string | null>(null);
+    const[location, setLocation]=useState<string | null>(null);
+    const[twitter,setTwitter]=useState<string | null>(null);
+    const [blog, setBlog]=useState<string | null>('');
+    const [company, setCompany]=useState<string | null>(null);
+    const[darkModeB, setDarkModeB]=useState<boolean>(true);
 
     useEffect(()=>{
         fetch(apiURL + searchedWord)
         .then((response) => response.json()
         .then((data)=>{
             setAvatar(data.avatar_url)
+            setName(data.login)
+            setJoined(data.created_at)
+            setBio(data.bio)
+            setRepos(data.public_repos)
+            setFollowers(data.followers)
+            setFollowing(data.following)
+            setLocation(data.location)
+            setTwitter(data.twitter_username)
+            setBlog(data.blog)
+            setCompany(data.company)
         })
         )
     },[])
